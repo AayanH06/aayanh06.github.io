8f84e8eb0b8436b4b94891a5a5b8d81c26acbac3
AayanH06 <aayan@hamdani.us>
2025-06-29 22:40:52 -0400

    replaced services w/ experience in the header

--- src/Header.tsx
+++ src/Header.tsx
@@ -8,7 +8,7 @@ const Header: React.FC = () => {
         <ul>
           <li><a href="#">Home</a></li>
           <li><a href="#">About</a></li>
-          <li><a href="#">Services</a></li>
+          <li><a href="#">Experience</a></li>
           <li><a href="#">Contact</a></li>
         </ul>
       </nav>
